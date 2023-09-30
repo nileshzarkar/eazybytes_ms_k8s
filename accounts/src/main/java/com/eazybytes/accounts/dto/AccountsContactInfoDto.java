741dd086524a227ebb5f68cf31bc0cceac08a062
nileshzarkar <nileshzarkar@gmail.com>
2023-09-30 19:05:01 +0530

    commit

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
@@ -1,11 +1,20 @@
 package com.eazybytes.accounts.dto;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class AccountsContactInfoDto {
+   
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
